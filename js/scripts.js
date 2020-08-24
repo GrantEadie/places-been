98e0226f12f01698540398c838960877c99e566d
Grant Eadie <grantleadie@gmail.com>
2020-08-24 10:33:57 -0700

    check it out dog Co-authored-by: Adam Mansell <adammansellhmb@gmail.com> Co-authored-by: Kolman Soifer <kolmansoifer@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,19 @@ Place.prototype.allAttributes = function() {
   return this.location + " " + this.date + " " + this.notes;
 }
 
-// let placeBeen = new PlaceBeen();
-// let place = new Place("Twisp", "2077", "Our last military stronghold before the lazer dragons.");
-// let place2 = new Place("An Island in the Sun", "4999", "A lovely place for a glass of white wine.");
-// let place3 = new Place("Sting's House", "August 7th, 1983", "We did too much acid.");
-// placeBeen.addPlace(place);
-// placeBeen.addPlace(place2);
-// placeBeen.addPlace(place3);
+let placeBeen = new PlaceBeen();
+let place = new Place("Twisp", "2077", "Our last military stronghold before the lazer dragons.");
+let place2 = new Place("An Island in the Sun", "4999", "A lovely place for a glass of white wine.");
+let place3 = new Place("Sting's House", "August 7th, 1983", "We did too much acid.");
+placeBeen.addPlace(place);
+placeBeen.addPlace(place2);
+placeBeen.addPlace(place3);
+
+
+
+//Co-authored-by: Kolman Soifer <ko
+
+
+ Co-autho
+
+//  Co-authored-by: Adam Mansell <adammansellhmb@gmail.com>
